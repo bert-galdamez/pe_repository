88276bfc7e5b58fe3f9c679a4524ad3494a88e82
Alberto Galdamez <alberto.galdamez@outlook.com>
2019-07-17 01:26:51 -0600

    Haciendo el segundo ejercicio

---
+++ Practica_Num1/Practica_Num1/Ejercicios/Ejercicio2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_Num1.Ejercicios
+{
+    class Ejercicio2
+    {
+        static void Main(string[] args)
+        {
+            //Variables
+            string nombre;
+            string apellido;
+            string cargo;
+            string correo;
+            short edad;
+            string fechaContratacion;
+            sbyte sueldo;
+
+            //Proceso: Introdccuion
+            Console.WriteLine("Recolección de dats de empleados");
+            Console.WriteLine("Presione [Enter] para continuar");
+            Console.ReadLine();
+
+
+            //Proceso: Recoleccion de datos
+            Console.Clear();
+            Console.WriteLine("Escriba el nombre del empleado");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Apellido");
+            apellido = Console.ReadLine();
+            Console.WriteLine("Cargo del empleado");
+            cargo = Console.ReadLine();
+
+            
+
+            
+        }
+    }
+}
