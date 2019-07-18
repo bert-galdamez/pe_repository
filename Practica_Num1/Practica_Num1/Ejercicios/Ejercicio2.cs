dc5e34efbb8a4a01c1b08b16c8f5af81c5771eac
Alberto Galdamez <alberto.galdamez@outlook.com>
2019-07-17 22:14:35 -0600

    Termine el segundo ejercicio

--- Practica_Num1/Practica_Num1/Ejercicios/Ejercicio2.cs
+++ Practica_Num1/Practica_Num1/Ejercicios/Ejercicio2.cs
@@ -16,7 +16,7 @@ namespace Practica_Num1.Ejercicios
             int mes;
             int anio;
             string fechaContratacion;
-            sbyte sueldo;
+            int sueldo;
 
             //Proceso: Introdccuion
             Console.WriteLine("Recolección de datos de empleados");
@@ -36,22 +36,43 @@ namespace Practica_Num1.Ejercicios
             Console.WriteLine();
             //Cargo
             Console.WriteLine("Cargo del empleado:");
-            Console.WriteLine();
             cargo = Console.ReadLine();
+            Console.WriteLine();
             //Edad
             Console.WriteLine("Edad del empleado:");
             edad = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
             //Correo
             Console.WriteLine("Correo del empleado:");
             correo = Console.ReadLine();
+            Console.WriteLine();
             //Fecha
             Console.WriteLine("Fecha de Contratación");
             Console.Write("Dia:");
             dia = Convert.ToInt32(Console.ReadLine());
             Console.Write("Mes:");
             mes = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Año");
+            Console.Write("Año:");
             anio = Convert.ToInt32(Console.ReadLine());
+            //Sueldo
+            Console.WriteLine();
+            Console.WriteLine("Sueldo:");
+            sueldo = Convert.ToInt32(Console.ReadLine());
+
+
+
+            //Muestra de datos:
+            Console.Clear();
+            Console.WriteLine("-----Datos del empleado-----");
+            Console.WriteLine("Nombre: {0}",nombre);
+            Console.WriteLine("Apellido: {0}", nombre);
+            Console.WriteLine("Cargo: {0}", cargo);
+            Console.WriteLine("Edad: {0}", edad);
+            Console.WriteLine("Correo del empleado: {0}", correo);
+            Console.WriteLine("Fecha de contratación: {0}/{1}/{2}", dia, mes, anio);
+            Console.WriteLine("Sueldo actual: {0}", sueldo);
+            Console.WriteLine("-----Finaliza tabla-----");
+            Console.ReadLine();
         }
     }
 }
