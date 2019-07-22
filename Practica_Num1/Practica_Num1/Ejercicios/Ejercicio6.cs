2c95d4c824e9c55320af08e7adb92e204531c8e6
Galdamez <alberto.galdamez@outlook.com>
2019-07-22 13:12:38 -0600

    Avance del ejercicio 6

--- Practica_Num1/Practica_Num1/Ejercicios/Ejercicio6.cs
+++ Practica_Num1/Practica_Num1/Ejercicios/Ejercicio6.cs
@@ -15,10 +15,14 @@ namespace Practica_Num1.Ejercicios
             string apellido;
             int sueldo;
 
-            const int AFP = 333;
-            const int renta = 22222;
-            const int ISSS = 0;
+            int afp;
+            int renta;
+            int isss;
 
+            //Constantes:
+            const double AFP = 13.74;
+            const double ISSS = 33.32;
+            const double RENTA = 24.75;
 
             //Proceso: Introducción al programa
 
@@ -29,14 +33,41 @@ namespace Practica_Num1.Ejercicios
 
             //Obtener datos
             Console.Write("Nombre del empleado: ");
-            Console.ReadLine();
+            nombre = Console.ReadLine();
             Console.Write("Apellido del empleado: ");
-            Console.ReadLine();
+            apellido = Console.ReadLine();
             Console.Write("Sueldo Actual: ");
             sueldo = Convert.ToInt32(Console.ReadLine());
 
 
             //Proceso: Convertir Datos
+            if(sueldo >= 526)
+            {
+                //Si el sueldo es mayor o igual a 526 se saca la renta.
+
+        
+            }
+
+            if (sueldo <= 525)
+            {
+
+                //Sacando AFP:
+                afp = Convert.ToInt32(sueldo / AFP);
+
+
+                //Sacando ISSS:
+                isss = Convert.ToInt32(sueldo / ISSS);
+
+                Console.Clear();
+                Console.WriteLine("Información:");
+                Console.WriteLine("Nombre Completo: {0} {1}", nombre, apellido);
+                Console.WriteLine("Sueldo Actual: {0}", sueldo);
+                Console.WriteLine("Descuento de AFP:${0}", afp);
+                Console.WriteLine("Descuento de ISS:${0}", isss);
+                Console.WriteLine("Debido a su salario, No paga renta");
+                Console.ReadKey();
+
+            }
 
 
 
