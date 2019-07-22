dcfaed2a4818fe3e1f59906c1e5e835d94d9ff5d
Galdamez <alberto.galdamez@outlook.com>
2019-07-22 08:48:55 -0600

    Sexto ejercicio

---
+++ Practica_Num1/Practica_Num1/Ejercicios/Ejercicio6.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_Num1.Ejercicios
+{
+    class Ejercicio6
+    {
+        static void Main(string[] args)
+        {
+            //Variables y arreglos.
+            string nombre;
+            string apellido;
+            int sueldo;
+
+            const int AFP = 333;
+            const int renta = 22222;
+            const int ISSS = 0;
+
+
+            //Proceso: Introducción al programa
+
+            Console.WriteLine("Sistema de datos de empleados");
+            Console.WriteLine("[Enter] para continuar");
+            Console.ReadKey();
+            Console.Clear();
+
+            //Obtener datos
+            Console.Write("Nombre del empleado: ");
+            Console.ReadLine();
+            Console.Write("Apellido del empleado: ");
+            Console.ReadLine();
+            Console.Write("Sueldo Actual: ");
+            sueldo = Convert.ToInt32(Console.ReadLine());
+
+
+            //Proceso: Convertir Datos
+
+
+
+
+
+        }
+    }
+}
